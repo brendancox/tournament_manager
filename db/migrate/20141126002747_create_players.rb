cf5b0b3f2d9bf026f89bf37aabb69c7058a2bb17
Brendan Cox <brendancox21@gmail.com>
2014-11-26 14:11:39 +1300

    Migration plus correction to migration file errors

--- db/migrate/20141126002747_create_players.rb
+++ db/migrate/20141126002747_create_players.rb
@@ -1,8 +1,8 @@
 class CreatePlayers < ActiveRecord::Migration
   def change
     create_table :players do |t|
-      t.first_name
-      t.last_name
+      t.string :first_name
+      t.string :last_name
 
       t.timestamps
     end
---
+++ db/schema.rb
@@ -0,0 +1,58 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20141126004553) do
+
+  create_table "activities", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "fixtures", force: true do |t|
+    t.integer  "tournament_id"
+    t.integer  "player1_id"
+    t.integer  "player1_score"
+    t.integer  "player2_id"
+    t.integer  "player2_score"
+    t.boolean  "completed"
+    t.string   "winner"
+    t.string   "current_stage"
+    t.integer  "next_playoff_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "participants", force: true do |t|
+    t.integer  "tournament_id"
+    t.integer  "player_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "players", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tournaments", force: true do |t|
+    t.string   "name"
+    t.integer  "activity_id"
+    t.integer  "num_of_players"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
