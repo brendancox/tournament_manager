5799a51458e10743c6aa93ebee0ab4fd8bd15af6
Brendan Cox <brendancox21@gmail.com>
2014-11-26 15:18:55 +1300

    Added Activity methods and views

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,26 @@
+class ActivitiesController < ApplicationController
+
+  def new
+  	@activity = Activity.new
+  end
+
+  def create
+  	activity = Activity.create(activity_params)
+  	redirect_to activity
+  end
+
+  def show
+  	@activity = Activity.find(params[:id])
+  end
+
+  def index
+  	@activities = Activity.all
+  end
+
+  private
+
+  def activity_params
+  	params.require(:activity).permit(:name)
+  end
+
+end
