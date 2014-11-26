aa8bbceb39a0057c34a8aed69b58bd0600f3363d
Brendan Cox <brendancox21@gmail.com>
2014-11-26 15:35:53 +1300

    Added Tournament Controller and Views

---
+++ app/controllers/tournaments_controller.rb
@@ -0,0 +1,27 @@
+class TournamentsController < ApplicationController
+
+  def new
+  	@tournament = Tournament.new
+  	@activity_select_options = Activity.all.pluck(:name, :id)
+  end
+
+  def create
+  	tournament = Tournament.create(tournament_params)
+  	redirect_to tournament
+  end
+
+  def show
+  	@tournament = Tournament.find(params[:id])
+  end
+
+  def index
+  	@tournaments = Tournament.all
+  end
+
+  private
+
+  def tournament_params
+  	params.require(:tournament).permit(:name, :activity_id, :num_of_players)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
 
-  resources :activities
+  resources :activities, :tournaments
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
