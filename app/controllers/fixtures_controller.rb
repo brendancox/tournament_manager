09bbcd464c87df60b71283133f98b382304f2736
Brendan Cox <brendancox21@gmail.com>
2014-12-31 01:04:42 +1300

    Can now add scores to fixtures

--- app/controllers/fixtures_controller.rb
+++ app/controllers/fixtures_controller.rb
@@ -6,10 +6,22 @@ class FixturesController < ApplicationController
 		@fixture = FixturesSet.new(@tournament, fixture).data[0]
 	end
 
-	def enter_score
-		fixture = Fixture.find(params[:id])
-		@tournament = Tournament.find(fixture.tournament_id)
-		@fixture = FixturesSet.new(@tournament, fixture).data[0]
-		@scores
+	def enter_result
+		@fixture_record = Fixture.find(params[:id])
+		@tournament = Tournament.find(@fixture_record.tournament_id)
+		@fixture = FixturesSet.new(@tournament, @fixture_record).data[0]
+	end
+
+	def record_result
+		fixture = Fixture.update(params[:id], record_result_params)
+		fixture.completed = true
+		fixture.save
+		redirect_to fixture
+	end
+
+	private 
+
+	def record_result_params
+		params.require(:fixture).permit(:player1_score, :player2_score)
 	end
 end
