0edaea2e832ddb8393d5454326dadcd35f081032
Brendan Cox <brendancox21@gmail.com>
2015-01-04 15:42:05 +1300

    Added testing for updateTournament model

---
+++ spec/models/update_tournament_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+describe "update rest of tournament" do
+	context "after first round" do
+		before do 
+			load_factories
+			@tournament = Tournament.first
+			@tournament.update(team_ids: [1, 2, 3, 4])
+			schedule = GenerateSchedule.new(@tournament)
+    	schedule.create
+		end
+
+		it "adds winning player ids to 2nd round game" do
+			fixture = Fixture.first
+			fixture.winner_id = fixture.player1_id
+			fixture.completed = true
+			fixture.save
+			update_competition_details = UpdateTournament.new(@tournament, fixture)
+			update_competition_details.apply_changes
+			expect(Fixture.last.player1_id).to eq(Fixture.first.winner_id)
+		end
+	end
+
+	context "after last round" do
+		before do 
+			load_factories
+			@tournament = Tournament.first
+			@tournament.update(team_ids: [1, 2])
+			schedule = GenerateSchedule.new(@tournament)
+    	schedule.create
+    	fixture = Fixture.first
+			fixture.winner_id = fixture.player1_id
+			fixture.completed = true
+			fixture.save
+			update_competition_details = UpdateTournament.new(@tournament, fixture)
+			update_competition_details.apply_changes
+		end
+
+		it "records winner of tournament" do
+			expect(@tournament.winner_id).not_to be_blank
+		end
+
+		it "records that tournament is complete" do
+			expect(@tournament.winner_id).to be_true
+		end
+	end
+end
