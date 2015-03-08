5a2506b9037f276be2c670f60a0af2ad9c78ec24
Brendan Cox <brendancox21@gmail.com>
2015-03-08 17:22:07 +1300

    Fixed change time functions

--- app/assets/javascripts/update_schedule.js
+++ app/assets/javascripts/update_schedule.js
@@ -2,15 +2,18 @@ $(document).on('ready page:load', function(){
 
 	$('.datetimepicker1').on('dp.change', function(){
 		this_entry = $(this);
-		fixture_id = this_entry.prop('id');
-		console.log(fixture_id);
-		new_time = this_entry.val();
-		$.ajax({
-			type:'put',
-			url: '/update_details',
-			data: {fixture: {id: fixture_id, time: new_time}},
-			dataType: 'json', 
-			success: console.log('success')
-		});
+		if (this_entry.hasClass('scheduleChangeTime'))
+		{
+			fixture_id = this_entry.prop('id');
+			console.log(fixture_id);
+			new_time = this_entry.val();
+			$.ajax({
+				type:'put',
+				url: '/update_details',
+				data: {fixture: {id: fixture_id, time: new_time}},
+				dataType: 'json', 
+				success: console.log('success')
+			});
+		}
 	});
 });
