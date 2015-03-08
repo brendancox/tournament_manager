1a120b17085271a83bb7ddeb2ccd40828d4b5415
Brendan Cox <brendancox21@gmail.com>
2015-03-08 22:41:40 +1300

    Updated bootstrap styling of app

--- app/assets/javascripts/new_tournament_form.js
+++ app/assets/javascripts/new_tournament_form.js
@@ -1,2 +1,12 @@
 $(document).on('ready page:load', function(){
+	$('#tournament_format').change(function(){
+		if ($('#tournament_format option:selected').text() === 'League then Playoffs')
+		{
+			$('#choose_num_in_playoffs').show();
+		}
+		else
+		{
+			$('#choose_num_in_playoffs').hide();
+		}
+	})
 });
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Activity.create([{name: 'Football'}, {name: 'Rugby'}, {name: 'Hockey'}, {name: 'Basketball'},
+	{name: 'Netball'}, {name: 'American Football'}])
